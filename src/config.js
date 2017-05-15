9f26933cb6154a743c96368b21452a1a63d1827e
dviramontes <dviramontes@gmail.com>
2017-05-14 20:02:28 -0600

    feat: adds simple gcal auth service routes

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+    'client_id': process.env.TIEMPO_CLIENT_ID,
+    'client_secret': process.env.TIEMPO_CLIENT_SECRET,
+    'project_id': process.env.TIEMPO_PROJECT_ID,
+    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+    'token_uri': 'https://accounts.google.com/o/oauth2/token',
+    'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,56 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import passport from 'passport';
+import gcal from 'google-calendar';
+import has from 'lodash.has';
+import cookieParser from 'cookie-parser';
+import passportGoogleOAuth from 'passport-google-oauth';
+import session from 'express-session';
+
+import config from './config';
 
 const app = express();
-app.get('/', (req, res) => res.send('it works!!'));
-app.listen(4000);
+const GoogleStrategy = passportGoogleOAuth.OAuth2Strategy;
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(session({ secret: 'FBI_CREDS' }));
+app.use(passport.initialize());
+
+const PORT = 4000;
+const scope = ['openid', 'email', 'https://www.googleapis.com/auth/calendar'];
+
+const GoogleStrategyConfig = new GoogleStrategy({
+    clientID: config.client_id,
+    clientSecret: config.client_secret,
+    callbackURL: `http://localhost:${PORT}/auth/callback`,
+    scope,
+}, (accessToken, refreshToken, profile, done) => {
+    profile.accessToken = accessToken;
+    return done(null, profile);
+});
+
+passport.use(GoogleStrategyConfig);
+
+app.get('/auth', passport.authenticate('google', {session: false}));
+
+app.get('/auth/callback', passport.authenticate('google',
+    {session: false, failureRedirect: '/login'}),
+    (req, res) => {
+        req.session.access_token = req.user.accessToken;
+        res.redirect('/');
+    });
+
+app.all('/', function (req, res) {
+
+    if (!has(req, 'session.access_token')) return res.redirect('/auth');
+
+    const accessToken = req.session.access_token;
+
+    gcal(accessToken).calendarList.list(function (err, data) {
+        if (err) return res.send(500, err);
+        return res.send(data);
+    });
+});
+
+app.listen(PORT);
