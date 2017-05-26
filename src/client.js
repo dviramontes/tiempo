1da86fb315f7aa2c652aa7a0249fbcd9183a302e
dviramontes <dviramontes@gmail.com>
2017-05-25 18:52:47 -0600

    chore: format

--- src/client.js
+++ src/client.js
@@ -1,10 +1,10 @@
 import join from 'lodash.join';
 import '../main.less';
 
-function component () {
-    const element = document.createElement('div');
-    element.innerHTML = join(['All', ' rolled up!'], '');
-    return element;
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = join(['All', ' rolled up!'], '');
+  return element;
 }
 
 document.body.appendChild(component());
--- src/server.js
+++ src/server.js
@@ -22,42 +22,42 @@ const PORT = 4000;
 const scope = ['openid', 'email', 'https://www.googleapis.com/auth/calendar'];
 
 const GoogleStrategyConfig = new GoogleStrategy({
-    clientID: config.client_id,
-    clientSecret: config.client_secret,
-    callbackURL: `http://localhost:${PORT}/auth/callback`,
-    scope,
+  clientID: config.client_id,
+  clientSecret: config.client_secret,
+  callbackURL: `http://localhost:${PORT}/auth/callback`,
+  scope,
 }, (accessToken, refreshToken, profile, done) => {
-    profile.accessToken = accessToken;
-    return done(null, profile);
+  profile.accessToken = accessToken;
+  return done(null, profile);
 });
 
 passport.use(GoogleStrategyConfig);
 
 app.all('/', (req, res) => {
-    if (!has(req, 'session.access_token')) return res.redirect('/auth');
-    return res.status(200).send(req.session.accessToken);
+  if (!has(req, 'session.access_token')) return res.redirect('/auth');
+  return res.status(200).send(req.session.accessToken);
 });
 
 app.all('/calendar/:id', (req, res) => {
-    if (!has(req, 'session.access_token')) return res.redirect('/auth');
-    const accessToken = req.session.access_token;
-    gcal(accessToken).calendarList.list((err, calendarList) => {
-        if (err) return res.status(500).send(err);
-        const calendarId = req.params.id;
-        gcal(accessToken).events.list(calendarId, (err, eventList) => {
-            return res.status(200).send(eventList);
-        });
+  if (!has(req, 'session.access_token')) return res.redirect('/auth');
+  const accessToken = req.session.access_token;
+  gcal(accessToken).calendarList.list((err, calendarList) => {
+    if (err) return res.status(500).send(err);
+    const calendarId = req.params.id;
+    gcal(accessToken).events.list(calendarId, (err, eventList) => {
+      return res.status(200).send(eventList);
     });
+  });
 });
 
 app.get('/auth', passport.authenticate('google', { session: false }));
 
-app.get('/auth/callback',
-    passport.authenticate('google',
-        { session: false, failureRedirect: '/login' }),
-    (req, res) => {
-        req.session.access_token = req.user.accessToken;
-        res.redirect('/');
-    });
+app.get('/auth/callback', passport.authenticate('google', {
+  session: false,
+  failureRedirect: '/login'
+}), (req, res) => {
+  req.session.access_token = req.user.accessToken;
+  res.redirect('/');
+});
 
 app.listen(PORT);
